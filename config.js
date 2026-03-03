40e99163e5c1377624d0cdc94608146135c6f5a0
kyoujyu88 <47493032+kyoujyu88@users.noreply.github.com>
2026-03-03 14:55:28 +0900

    Create config.js with site URL and list name
    Add configuration file with site URL and list name.

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// ★篤志さんの環境に合わせて書き換えてくださいねっ★
+
+// サイトのURL (Listsの前までです！)
+const SITE_URL = "https://na-n.gbase.gsdf.mod.go.jp/na/NA/NAFin/HQ/sinsa";
+
+// リストの内部名 (Title列だけのテスト用リストです)
+const LIST_NAME = "TestList"; 
